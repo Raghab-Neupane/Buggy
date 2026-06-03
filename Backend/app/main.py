28aa04b2979fe97d21aa809fd2603e2d901609d2
RAGHAB NEUPANE <neupaneraghab@gmail.com>
2026-06-03 11:49:45 +0545

    changed the backend routings

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,50 +1,17 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI
+from app.schema.schema import LogEvent
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 logs = []
-connections = []
-
 
 @app.post("/logs")
-def create_log(log: dict):
-    logs.append(log)
-    return {"success": True}
+async def create_log(log: LogEvent):
+    logs.append(log.model_dump())
+    return {
+        "Status": "Sucess"
+    }
 
-
-@app.get("/dashboard")
-def get_logs():
+@app.get("/logs")
+async def get_logs():
     return logs
-
-async def broadcast_log(message: str):
-    for connection in connections:
-        await connection.send_text(message)
-
-@app.get("/test")
-async def test():
-    print("Route hit")
-
-    await broadcast_log("Hello from Python!")
-
-    return {"success": True}
-
-
-@app.websocket("/ws/logs")
-async def websocket_logs(websocket: WebSocket):
-
-    await websocket.accept()
-
-    while True:
-        await websocket.send_text("hello")
-        import asyncio
-        await asyncio.sleep(1)
